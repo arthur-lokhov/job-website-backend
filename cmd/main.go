320b6457455c2ebac2b2000ce5cfe5106b3bb6ab
Михаил Нечаев <nechaev@cyberzone.dev>
2025-05-04 00:00:15 +0300

    update: main

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
 	"git.cyberzone.dev/project-vacancy-website/job-website-backend/internal/config"
 	"git.cyberzone.dev/project-vacancy-website/job-website-backend/internal/db"
+	"git.cyberzone.dev/project-vacancy-website/job-website-backend/internal/vacancy"
 )
 
 func main() {
 	cfg := config.LoadConfig()
+
 	db.Init(cfg)
 
-	fmt.Println("✅ Connected to PostgreSQL via GORM!")
+	r := mux.NewRouter()
+
+	// Vacancy endpoints
+	r.HandleFunc("/api/vacancies", vacancy.CreateVacancy).Methods("POST")
+	r.HandleFunc("/api/vacancies", vacancy.GetAllVacancies).Methods("GET")
+
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/spec v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.1 // indirect
+	github.com/gorilla/mux v1.8.1
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.4 // indirect
